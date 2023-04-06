c411925fb5c5e57681e3a6f09ce600b639ba7870
Saar Shen <xiaomi7732@hotmail.com>
2023-04-06 15:43:44 -0700

    Add an option to disable process start trigger

--- src/DotNet.Diagnostics.Counters.ProcessStartTrigger/ProcessStartTriggerOptions.cs
+++ src/DotNet.Diagnostics.Counters.ProcessStartTrigger/ProcessStartTriggerOptions.cs
@@ -3,5 +3,6 @@ namespace DotNet.Diagnostics.Counters.Triggers;
 public class ProcessStartTriggerOptions
 {
     public const string DefaultSectionName = "ProcessStart";
+    public bool IsEnabled { get; set; } = true;
     public TimeSpan InitialDelay { get; set; } = TimeSpan.FromSeconds(5);
 }
